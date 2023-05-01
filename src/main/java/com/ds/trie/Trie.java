95951c5dcfe98e2ea982e7aa455e9b4bdb92bb6c
Sujeet <sujeetgupta16@gmail.com>
2023-05-01 20:07:01 +0530

    Trie adding commentary

--- src/main/java/com/ds/trie/Trie.java
+++ src/main/java/com/ds/trie/Trie.java
@@ -1,5 +1,7 @@
 package com.ds.trie;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +18,8 @@ public class Trie {
     }
 
     private void insertWord(TrieNode node, String word) {
-
-        if (word.length() == 0) {
+        //base case , if no word left, then set is terminal true and return
+        if (StringUtils.isEmpty(word)) {
             node.isTerminal = true;
             return;
         }
@@ -28,7 +30,7 @@ public class Trie {
             child = new TrieNode(character);
             node.children.put(character, child);
         }
-
+        //insert next character in the string in the child node
         insertWord(child, word.substring(1));
     }
 
@@ -38,15 +40,19 @@ public class Trie {
     }
 
     private boolean searchWord(TrieNode node, String word) {
-        if (word.length() == 0) {
+        //if the world is empty, then check if status is terminal node status
+        //whether it is present or not
+        if (StringUtils.isEmpty(word)) {
             return node.isTerminal;
         }
 
         Character character = word.charAt(0);
         TrieNode child = node.children.get(character);
+        //if not child node, then return false, as it is not present
         if (child == null) {
             return false;
         }
+        //search for next character in the string in the child node
         return searchWord(child, word.substring(1));
     }
 
@@ -55,11 +61,14 @@ public class Trie {
     }
 
     private boolean deleteWord(TrieNode node, String word) {
-        if (word.length() == 0) {
+        if (StringUtils.isEmpty(word)) {
+            //if node is terminal then, mark deleted as true, because that means it was present
+            //and set the node status as not terminal, as other node, will be there down in the hierarchy
             if (node.isTerminal) {
                 node.isTerminal = false;
                 return true;
             }
+            //if it is not terminal then return false, becasue tht means it was not present
             return false;
         }
 
@@ -68,8 +77,10 @@ public class Trie {
         if (child == null) {
             return false;
         }
+        //delete from child node for next character
         boolean deleted = deleteWord(child, word.substring(1));
 
+        //if it is successfully deleted, and it was the only node present, then reclaim that space
         if (deleted && node.children.size() == 1) {
             node.children.remove(character);
             return true;
@@ -82,27 +93,21 @@ public class Trie {
         List<String> recommendation = new ArrayList<>();
         TrieNode prefixNode = getPrefixNode(prefix);
         computeSuggestedWords(prefixNode, retWordCount, recommendation, prefix);
-
         return recommendation;
-
     }
 
     private void computeSuggestedWords(TrieNode node, int retWordCount,
                                        List<String> recommendation, String currentWord) {
-
         if (node.isTerminal) {
             recommendation.add(currentWord);
             return;
         }
-
         if (recommendation.size() >= retWordCount) {
             return;
         }
-
         for (TrieNode child : node.children.values()) {
             computeSuggestedWords(child, retWordCount, recommendation, currentWord + child.data);
         }
-
     }
 
 
@@ -113,7 +118,6 @@ public class Trie {
             Character character = prefix.charAt(i++);
             node = node.children.get(character);
         }
-
         return node;
     }
 
